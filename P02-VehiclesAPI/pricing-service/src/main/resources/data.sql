f92ec32e322de46b924749d5e039088381f2f345
svolta@itx.com <svolta@git.com>
2021-11-05 17:00:30 -0300

    Fixing eureka server dependency. Adding pricing sql to add data

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (id, currency, price) VALUES (1, 'USD', 21904.12);
+INSERT INTO price (id, currency, price) VALUES (2, 'USD', 18205.15);
+INSERT INTO price (id, currency, price) VALUES (3, 'USD', 30405.10);
+INSERT INTO price (id, currency, price) VALUES (4, 'USD', 12201.11);
+INSERT INTO price (id, currency, price) VALUES (5, 'USD', 19104.40);
